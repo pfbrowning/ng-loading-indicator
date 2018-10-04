509b208877b367167165f3b304a2fd173e35c269
Patrick Browning <patrick@browninglogic.com>
2018-10-03 22:44:59 -0400

    Update demo to demonstrate changing the header text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoadingIndicatorService} from 'ng-loading-indicator'
 import { timer } from 'rxjs';
+import { tap, switchMap, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,25 @@ export class AppComponent {
   public show(): void {
     // Show the loading indicator
     this.loadingIndicatorService.showLoadingIndicator();
-    // Hide the loading indicator after two seconds
-    timer(2000).subscribe(() => this.loadingIndicatorService.hideLoadingIndicator());
+    // Simulate a two second API call
+    timer(2000)
+      /* Hide the loading indicator after the completion of the API call. */
+      .pipe(finalize(() => this.loadingIndicatorService.hideLoadingIndicator()))
+      .subscribe();
+  }
+
+  public showDifferentMessages(): void {
+    // Show the loading indicator with custom loading message text
+    this.loadingIndicatorService.showLoadingIndicator("Fetching the first thing");
+    // Simulate a two second API call
+    timer(2000).pipe(
+      // Change the loading message text between the first and second simulated API call
+      tap(() => this.loadingIndicatorService.showLoadingIndicator("Fetching something else")),
+      // Simulate a second two second API call
+      switchMap(() => timer(2000)),
+      /* Hide the loading indicator upon completion of both of the simulated API 
+      calls, regardless of whether the operation succeeded or failed. */
+      finalize(() => this.loadingIndicatorService.hideLoadingIndicator())
+    ).subscribe();
   }
 }
